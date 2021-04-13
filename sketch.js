5770729d6bd01839cb3199cbd5a5be2ea1254709
taylorzukowski <79670105+taylorzukowski@users.noreply.github.com>
2021-04-13 13:59:50 -0700

    update

--- sketch.js
+++ sketch.js
@@ -25,12 +25,11 @@ function keyTyped() {
   } else if (key === 'd') {
     background(255);
     beginShape();
-    for (let i = 0; i < array.length; i++) {
+    for(let i = 0; i < array.length; i++) {
       //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
       curveVertex(array[i][0], array[i][1])
     }
     endShape();
   }
-  return;
-  false
+  return false;
 }
