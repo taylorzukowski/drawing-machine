046d22cd07960505fc42c614073c84ff2f90d2d1
taylorzukowski <79670105+taylorzukowski@users.noreply.github.com>
2021-04-22 11:39:12 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,21 +1,28 @@
-//let array = [];
-let noiseOffset = 0.0;
-let strokeWidth = 5;
+let array = [];
+
+//let noiseOffset = 0.0;
+//let strokeWidth = 5;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(129, 174, 247);
+  //background(129, 174, 247);
 
-
+  drawGrid();
+  strokeWeight (5);
   noFill();
+
 }
+
+  drawGrid();
+  strokeWeight (5);
+  noFill();
 
 function draw() {
 
-  background(129, 174, 247, 20);
-strokeWeight(strokeWidth);
+//  background(129, 174, 247, 20);
+//strokeWeight(strokeWidth);
 
-noiseOffset += 0.05;
-strokeWidth = noise(noiseOffset) * 100;
+//noiseOffset += 0.05;
+//strokeWidth = noise(noiseOffset) * 100;
 
 
   if (mouseIsPressed) {
@@ -40,5 +47,24 @@ function keyTyped() {
       curveVertex(array[i][0], array[i][1])
     }
     endShape();
+  }
+
+  function drawGrid(){
+    numCells = 20;
+    fillColor = 255;
+    strokeWeight(0);
+    for (let i = 0; i <= width; i += width / numCells){
+      for (let j = 0; j <= height; j += height / numCells){
+        if (fillColor === 255){
+          fillColor = 200;
+        } else {
+          fillColor = 255;
+        }
+        fill(fillColor);
+        rect(i, j, width / numCells, height / numCells);
+      }
+
+    }
+    strokeWeight(5);
   }
 //  return false;
