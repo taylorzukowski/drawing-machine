64290db4d1ca41d8ddce1e9201f620cbe237e38c
taylorzukowski <79670105+taylorzukowski@users.noreply.github.com>
2021-04-22 11:07:22 -0700

    Update

--- sketch.js
+++ sketch.js
@@ -1,20 +1,27 @@
-let array = [];
-
-
+//let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(225);
+  background(129, 174, 247);
 
-  strokeWeight(5);
+
   noFill();
 }
 
 function draw() {
 
+  background(129, 174, 247, 20);
+strokeWeight(strokeWidth);
+
+noiseOffset += 0.05;
+strokeWidth = noise(noiseOffset) * 100;
+
+
   if (mouseIsPressed) {
-    //  line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
-    array.push([mouseX, mouseY]);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  //  background(0);
+  //  array.push([mouseX, mouseY]);
   }
 }
 
@@ -23,14 +30,15 @@ function keyTyped() {
   if (key === 's') {
     // save this image
     saveCanvas('fileName', 'png');
-  } else if (key === 'd') {
-    background(255);
-    beginShape();
-    for(let i = 0; i < array.length; i++) {
+  } else if (key === 'c') {
+    clear();
+  //} else if (key === 'd') {
+    //background(255);
+    //beginShape();
+  //  for(let i = 0; i < array.length; i++) {
       //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
       curveVertex(array[i][0], array[i][1])
     }
     endShape();
   }
-  return false;
-}
+//  return false;
